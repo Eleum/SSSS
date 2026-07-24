b0d7c61dd5bd8405138a883340d18a12fbba879e
Eleum <wotlk777@gmail.com>
2026-07-24 22:57:34 +0200

    Restore 10 backups

--- StoneShardSaveScumming.Domain/Backups/BackupOptions.cs
+++ StoneShardSaveScumming.Domain/Backups/BackupOptions.cs
@@ -1,4 +1,4 @@
 ﻿namespace StoneShardSaveScumming.Domain.Backups
 {
-    public sealed record BackupOptions(int MaxBackups = int.MaxValue);
+    public sealed record BackupOptions(int MaxBackups = 10);
 }
